cb040a2ef1525c6b91078629e6db9d16e7adf805
Santiago548 <dominicbarajas548@gmail.com>
2021-03-19 16:03:30 -0600

    commit tree d124443266dddee35afac263c48df35f65de90e6

--- index.js
+++ index.js
@@ -20,3 +20,85 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let createEmployeeRecord = function(data){
+    return {
+    firstName: data[0],
+    familyName: data[1],
+    title: data[2],
+    payPerHour: data[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(arr){
+    return  arr.map(function(data){
+        return createEmployeeRecord(data)
+    })
+}
+
+let createTimeInEvent = function(dateTime){
+    let [date, hour] = dateTime.split(' ')
+
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        date,
+        hour: parseInt(hour, 10),
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateTime){
+    let [date, hour] = dateTime.split(' ')
+
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        date,
+        hour: parseInt(hour, 10),
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(workTime){
+    let timeInEvent = this.timeInEvents.find(function(e){
+        return e.date === workTime
+    })
+
+    let timeOutEvent = this.timeOutEvents.find(function(e){
+        return e.date === workTime
+    })
+
+    return (timeOutEvent.hour - timeInEvent.hour) / 100 
+}
+
+let wagesEarnedOnDate = function(dateTime){
+    let workedDates = hoursWorkedOnDate.call(this, dateTime)
+    * this.payPerHour
+    return parseFloat(workedDates.toString())
+}
+
+let WagesFor = function(){
+    let workedDates = this.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let pay = workedDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
+
+    return pay    
+}
+
+
+let findEmployeeByFirstName = function(employees, firstName){
+    return employees.find(function(src){
+        return src.firstName === firstName
+    })
+}
+
+let calculatePayroll = function(arrOfEmployee){
+    return arrOfEmployee.reduce(function(memo, pay){
+        return memo + allWagesFor.call(pay)
+    }, 0)
+}
